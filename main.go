942531882994cee9a570c341805ce45aaffaff0c
NRG1989 <onezhkin@gmail.com>
2022-08-04 01:21:45 +0300

    first init

---
+++ main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"contact-recover/app"
+	"contact-recover/controllers"
+	"fmt"
+	"net/http"
+	"os"
+
+	"github.com/gorilla/mux"
+)
+
+func main() {
+
+	router := mux.NewRouter()
+
+	router.HandleFunc("/api/user/new",
+		controllers.CreateAccount).Methods("POST")
+
+	router.HandleFunc("/api/user/login",
+		controllers.Authenticate).Methods("POST")
+
+	router.HandleFunc("/api/me/contacts",
+		controllers.GetContactsFor).Methods("GET")
+	router.Use(app.JwtAuthentication) // добавляем middleware проверки JWT-токена
+
+	port := os.Getenv("PORT") //Получить порт из файла .env; мы не указали порт, поэтому при локальном тестировании должна возвращаться пустая строка
+	if port == "" {
+		port = "8000" //localhost
+	}
+
+	fmt.Println(port)
+
+	err := http.ListenAndServe(":"+port, router) //Запустите приложение, посетите localhost:8000/api
+
+	if err != nil {
+		fmt.Print(err)
+	}
+}
